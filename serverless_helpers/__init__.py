c30181eed55cc1f2af6da4ee8608f4f2052ceb38
Ryan S. Brown <sb@ryansb.com>
2016-03-15 13:37:57 -0400

    Document calling with __file__ as starting env path

--- serverless_helpers/__init__.py
+++ serverless_helpers/__init__.py
@@ -6,11 +6,18 @@ from dotenv import load_dotenv, get_key, set_key, unset_key
 def load_envs(path):
     """Recursively load .env files starting from `path`
 
-    Given the path "foo/bar/.env" and a directory structure like:
+    Usage: from your Lambda function, call load_envs with the value __file__ to
+    give it the current location as a place to start looking for .env files.
+
+    import serverless_helpers
+    serverless_helpers.load_envs(__file__)
+
+    Given the path "foo/bar/myfile.py" and a directory structure like:
         foo
         \---.env
         \---bar
             \---.env
+            \---myfile.py
 
     Values from foo/bar/.env and foo/.env will both be loaded, but values in
     foo/bar/.env will take precedence over values from foo/.env
