7ca17e79f8c3dba7bc04377e7746a383a281562d
Ryan S. Brown <sb@ryansb.com>
2016-03-15 13:09:52 -0400

    Add `load_envs` to take a base path and recurse up, looking for .envs

--- serverless_helpers/__init__.py
+++ serverless_helpers/__init__.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
 # MIT Licensed, Copyright (c) 2016 Ryan Scott Brown <sb@ryansb.com>
 
-import dotenv
+from dotenv import load_dotenv, get_key, set_key, unset_key
+
+def load_envs(path):
+    import os
+    path, _ = os.path.split(path)
+    if path == '/':
+        # bail out when you reach top of the FS
+        load_dotenv(os.path.join(path, '.env'))
+        return
+    # load higher envs first
+    # closer-to-base environments need higher precedence.
+    load_envs(path)
+    load_dotenv(os.path.join(path, '.env'))
