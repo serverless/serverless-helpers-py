d41f73e464053859e19a2134916cf63e5536da0c
Ryan S. Brown <sb@ryansb.com>
2016-03-19 07:50:22 -0400

    Only export necessary symbols

--- serverless_helpers/__init__.py
+++ serverless_helpers/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # MIT Licensed, Copyright (c) 2016 Ryan Scott Brown <sb@ryansb.com>
 
+__all__ = ['load_dotenv', 'get_key', 'set_key', 'unset_key', 'load_envs', 'load_cfn_outputs']
+
 import os
 import logging
 logger = logging.getLogger()
@@ -27,14 +29,23 @@ def load_envs(path):
     Values from foo/bar/.env and foo/.env will both be loaded, but values in
     foo/bar/.env will take precedence over values from foo/.env
     """
-    import os
     path = os.path.abspath(path)
     path, _ = os.path.split(path)
+
+
     if path == '/':
         # bail out when you reach top of the FS
-        load_dotenv(os.path.join(path, '.env'))
+        _maybe_load(os.path.join(path, '.env'))
         return
     # load higher envs first
     # closer-to-base environments need higher precedence.
     load_envs(path)
-    load_dotenv(os.path.join(path, '.env'))
+    _maybe_load(os.path.join(path, '.env'))
+
+
+def _maybe_load(env):
+    if os.path.isfile(env):
+        logger.debug("Loading .env file %s" % env)
+        load_dotenv(env)
+    else:
+        logger.info(".env file %s does not exist, not loading." % env)
